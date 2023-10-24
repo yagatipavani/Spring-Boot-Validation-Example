a06d832d36cff8feee9eddd497b55b0d5d2cacfd
yagatipavani <yagatipavani123@gmail.com>
2023-10-24 15:17:14 +0530

    init repo

---
+++ src/test/java/com/example/SpringBootValidationExample/SpringBootValidationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootValidationExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
